99dcb380858fba844b6a6347f1e648285b47636c
Sander Lysakermoen <slysakermoen@gmail.com>
2022-06-08 10:24:10 +0200

    Update Oppgave1.py

--- Oppgave1.py
+++ Oppgave1.py
@@ -1,27 +1,27 @@
 def bestPath(matrix):
-    #Henter ut x og y lengden på matrisen og lagrer dem. Dette er ikke nødvendig men gjør koden mer oversiktelig
-    xlen = len(matrix)    # row
+	#Henter ut x og y lengden på matrisen og lagrer dem. Dette er ikke nødvendig men gjør koden mer oversiktelig
+	xlen = len(matrix)    # row
 	ylen = len(matrix[0]) # column
 
-    #Her looper vi gjennom arrayen, først på x aksen, også på y aksen. 2 nested for loops på denne måten gjør at vi går igjennom alle elementene i 2D arrayen.
+	#Her looper vi gjennom arrayen, først på x aksen, også på y aksen. 2 nested for loops på denne måten gjør at vi går igjennom alle elementene i 2D arrayen.
 	for x in range(0, xlen):
 		for y in range(0, ylen):
-            #Om vi er på y 0 og x 0 vil vi hoppe over dette elementet.
+			#Om vi er på y 0 og x 0 vil vi hoppe over dette elementet.
 			if y == 0 and x == 0:
 				continue
-            #Her sjekker vi om x er 0. Dette betyr at vi er helt inntil kanten på matrixen. Om vi er det, har vi ikke noe valg, så vi velger den eneste vi kan nå, som er på koordinatet x, y-1. Dette vil si at det er på samme x akse som elementet valgt av loopen, men den er 1 y verdi mindre.
+			#Her sjekker vi om x er 0. Dette betyr at vi er helt inntil kanten på matrixen. Om vi er det, har vi ikke noe valg, så vi velger den eneste vi kan nå, som er på koordinatet x, y-1. Dette vil si at det er på samme x akse som elementet valgt av loopen, men den er 1 y verdi mindre.
 			elif x == 0:
 				matrix[x][y] += matrix[x][y-1]
-            #Her sjekker vi motsatt, om den er intill y-veggen, legger vi til den som er mot x.
+			#Her sjekker vi motsatt, om den er intill y-veggen, legger vi til den som er mot x.
 			elif y == 0:
 				matrix[x][y] += matrix[x-1][y]
-            #Om vi faktisk har noen valg som ikke bryter reglene, vil vi velge det laveste. Om verdien på koordinatet x-1, y er lavest, velger vi den. Om ikke, betyr det at x, y-1 er enten lavere eller den samme verdien, og da velger vi bare den.
+			#Om vi faktisk har noen valg som ikke bryter reglene, vil vi velge det laveste. Om verdien på koordinatet x-1, y er lavest, velger vi den. Om ikke, betyr det at x, y-1 er enten lavere eller den samme verdien, og da velger vi bare den.
 			else:
 				if matrix[x-1][y] < matrix[x][y-1]:
 					matrix[x][y] += matrix[x-1][y]
 				else:
 					matrix[x][y] += matrix[x][y-1]
-    #Etter vi er ferdig med hele loopen, vil verdien av det siste elementet, altså nederst i høyre hjørne være summen vår. Denne returnerer vi.
+	#Etter vi er ferdig med hele loopen, vil verdien av det siste elementet, altså nederst i høyre hjørne være summen vår. Denne returnerer vi.
 	return matrix[ylen-1][xlen-1]
 
 #Inverse funksjonen reverserer alle verdiene ved å gange de med -1. Dette vil da si at de største tallene blir de minste, og de minste tallene blir de største.
